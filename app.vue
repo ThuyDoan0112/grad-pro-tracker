c9a203aa2052f076b4445894f5221d68f8e0c22f
ThuyDoan0112 <doanthuy986@gmail.com>
2023-06-17 14:45:58 +0700

    :sparkles: Working with attribute binding

--- app.vue
+++ app.vue
@@ -1,9 +1,14 @@
 <script setup>
+import appLogo from "./assets/nuxt-logo.png";
+
 const appName = "Grad Pro Tracker";
 </script>
 <template>
   <header class="app-header">
-    <h1 class="name">{{ appName }}</h1>
+    <div class="app-info">
+      <img :src="appLogo" alt="App logo" class="logo" />
+      <h1 class="name">{{ appName }}</h1>
+    </div>
   </header>
 </template>
 <style scoped>
@@ -11,8 +16,17 @@ const appName = "Grad Pro Tracker";
   padding: 16px 32px;
   background-color: rgb(38, 127, 182);
 }
-.app-header > .name {
+.app-header > .app-info {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+}
+.app-header > .app-info > .name {
   color: #ffffff;
   margin: 0;
+}
+.app-header > .app-info > .logo {
+  height: 48px;
+  width: 48px;
 }
 </style>
