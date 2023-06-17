38fdaf08041ec2ca6403e68f83c6ab06abf813f8
ThuyDoan0112 <doanthuy986@gmail.com>
2023-06-17 14:21:58 +0700

    :sparkles: Working with template binding

--- app.vue
+++ app.vue
@@ -1,5 +1,18 @@
+<script setup>
+const appName = "Grad Pro Tracker";
+</script>
 <template>
-  <div>
-    <NuxtWelcome/>
-  </div>
+  <header class="app-header">
+    <h1 class="name">{{ appName }}</h1>
+  </header>
 </template>
+<style scoped>
+.app-header {
+  padding: 16px 32px;
+  background-color: rgb(38, 127, 182);
+}
+.app-header > .name {
+  color: #ffffff;
+  margin: 0;
+}
+</style>
